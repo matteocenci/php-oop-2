ed9ef92190c8d17157dde01ba36586bbe918a5fe
matteocenci <matteocenci01@gmail.com>
2024-06-03 17:14:15 +0200

    feat: var_dump for products

--- index.php
+++ index.php
@@ -7,6 +7,17 @@ $dogCategory = new Category("Cani", "C");
 var_dump($catCategory);
 var_dump($dogCategory);
 
-$dogFood = new Product("Crocchette", 21.99, $dogCategory, "Immagine");
+$dogFood = new Product("Crocchette per cani", 21.99, $dogCategory, "Immagine");
+$catFood = new Product("Crocchette per gatti", 21.99, $catCategory, "Immagine");
+$dogToy = new Product("Giocattolo per cani", 9.99, $dogCategory, "Immagine");
+$catToy = new Product("Giocattolo per gatti", 9.99, $catCategory, "Immagine");
+$dogBed = new Product("Cuccia per cani", 39.99, $dogCategory, "Immagine");
+$catBed = new Product("Cuccia per gatti", 39.99, $catCategory, "Immagine");
+
 var_dump($dogFood);
+var_dump($catFood);
+var_dump($dogToy);
+var_dump($catToy);
+var_dump($dogBed);
+var_dump($catBed);
 ?>
